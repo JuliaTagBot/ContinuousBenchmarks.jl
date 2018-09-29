42aca250accb19e2f36842fa8ec7e788e4e3d6ac
mohamed82008 <mohamed82008@gmail.com>
2018-09-29 14:16:46 +1000

    remove Mamba benchmarks

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,35 @@
 using TuringBenchmarks
 using Test
 
-filenames_to_test = ["bernoulli.run.jl"]
+function tobenchmark(filename)
+    #filenames_to_bench = ["bernoulli.run.jl"]
+    filenames_to_bench = ["bernoulli.run.jl"]
+    if filename ∈ filenames_to_bench
+        return true
+    else
+        return false
+    end
+end
+
+mamba_benchmarks = ["binomial.run.jl", 
+                    "dyes.run.jl", 
+                    "gdemo-gewke.run.jl", 
+                    "kid.run.jl", 
+                    "school8.run.jl", 
+                    "sv.run.jl"]
+
+function nottobenchmark(filename)
+    if filename ∈ mamba_benchmarks
+        return false
+    else
+        return true
+    end
+end
 
 TURING_HOME = joinpath(@__DIR__, "..")
 for (root, dirs, files) in walkdir(TuringBenchmarks.BENCH_DIR)
     for file in files
-        if file ∈ filenames_to_test && splitext(file)[2] == ".jl"
+        if tobenchmark(file) && !nottoenchmark(file) && splitext(file)[2] == ".jl"
             filepath = abspath(joinpath(root, file))
             println("Testing `$file` ... ")
             job = `julia -e " cd(\"$(replace(pwd(), "\\"=>"\\\\"))\"); 
