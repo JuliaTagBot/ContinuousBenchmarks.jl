8fa8c322989c3d96284c4231184e406623d5f7ed
KDr2 <zhuo@hexoasis.com>
2019-06-28 16:00:29 +0800

    delete Manifest.toml

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,21 +1,9 @@
 using Pkg
-project_root = (@__DIR__) |> dirname
-
-for _ in 1:2
-    try
-        Pkg.activate()
-        Pkg.resovle()
-    catch
-    end
-end
-
-# run(`git clone https://github.com/TuringLang/Turing.jl.git ../Turing.jl`)
-# try pkg"develop ../Turing.jl" catch end
-# try pkg"develop ../Turing.jl" catch end
 
 using TuringBenchmarks
 using TuringBenchmarks.Runner
 
+project_root = (@__DIR__) |> dirname
 set_project_path(project_root)
 
 for bm_file in get_benchmark_files()
