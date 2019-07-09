c63633428f4598d66d1264f015f49b90bdda23c1
KDr2 <zhuo@hexoasis.com>
2019-07-09 09:05:06 +0800

    configurable comment templates

--- src/ContinuousBenchmarks.jl
+++ src/ContinuousBenchmarks.jl
@@ -6,7 +6,7 @@ using JSON
 
 export @benchmarkd,
     set_project_path,
-    set_benchmark_files,
+    set_benchmark_config_file,
     get_benchmark_files
 
 include("config.jl")
@@ -15,24 +15,25 @@ using .Utils
 
 const BENCH_DIR = abspath(joinpath(@__DIR__, "..", "benchmarks"))
 const PROJECT_PATH = Base.RefValue{Union{Nothing, String}}(nothing)
-const EXTERNAL_BENCHMARKS_SOURCE = Base.RefValue{Union{Nothing, String}}(nothing)
+const BENCHMARK_CONFIG_FILE = Base.RefValue{Union{Nothing, String}}(nothing)
+COMMENT_TEMPLATES = Dict{Symbol, String}()
 
 function set_project_path(project_path::String)
     PROJECT_PATH[] = project_path
 end
 
-function set_benchmark_files(source::String)
-    EXTERNAL_BENCHMARKS_SOURCE[] = source
+function set_benchmark_config_file(source::String)
+    BENCHMARK_CONFIG_FILE[] = source
 end
 
 function get_benchmark_files()
-    if EXTERNAL_BENCHMARKS_SOURCE[] != nothing
+    if BENCHMARK_CONFIG_FILE[] != nothing
         try
-            source = EXTERNAL_BENCHMARKS_SOURCE[]
+            source = BENCHMARK_CONFIG_FILE[]
             eval(Meta.parse("""include("$source")"""))
             return BENCHMARK_FILES
         catch ex
-            @warn "No benchmarks defined in $(EXTERNAL_BENCHMARKS_SOURCE[])"
+            @warn "No benchmarks defined in $(BENCHMARK_CONFIG_FILE[])"
         end
         return []
     else
